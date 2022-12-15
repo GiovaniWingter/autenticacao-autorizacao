ddbb8010bbded2df6e2461ff1d37e4b6dc79d477
GiovaniWingter <giovani_wingter@yahoo.com.br>
2022-12-15 09:54:07 -0300

    Alteração connection-factory

--- config/connection-factory.js
+++ config/connection-factory.js
@@ -3,10 +3,10 @@ var mysql = require("mysql");
 
 module.exports = function(){
  return mysql.createConnection({
-    host: "localhost",
+    host: "containers-us-west-157.railway.app",
     user: "root",
-    password: "123456",
-    database: "autenticacao",
-    port: 3305
+    password: "j9UM57eojqyR9IdfyLPw",
+    database: "railway",
+    port: 7083
   });
 } 
