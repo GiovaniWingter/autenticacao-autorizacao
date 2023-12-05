198e96309d69493e1d13c80a924fbf9d9e5baa5b
giovaniWingter <giovani_wingter@yahoo.com.br>
2023-12-05 13:30:37 -0300

    conn

--- config/connection-factory.js
+++ config/connection-factory.js
@@ -2,12 +2,20 @@ var mysql = require("mysql2");
 const dotenv = require('dotenv');
 dotenv.config();
 
-module.exports = function(){
- return mysql.createConnection({
+module.exports = function () {
+  try {
+    let conexao = mysql.createConnection({
     host: process.env.MYSQL_ADDON_HOST,
     user: process.env.MYSQL_ADDON_USER,
     password: process.env.MYSQL_ADDON_PASSWORD,
     database: process.env.MYSQL_ADDON_DB,
     port: process.env.MYSQL_ADDON_PORT
   });
+  console.log("Conexão estabelecida!");
+  return conexao;
+} catch (e) {
+  console.log("Falha ao estabelecer a conexão!");
+  console.log(e);
+  return null
+}
 } 
